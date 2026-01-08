4e003b2905ac41afdc0c018a54076c903b549c6c
Taekyung Ahn <46814961+gaetokk@users.noreply.github.com>
2026-01-08 23:38:17 +0900

    Title: 사각형의 총 넓이 2, Time: 57ms, Memory: 17MB, Status: Passed - Codetree

--- 260108/사각형의 총 넓이 2/total-width-of-a-rectangle2.py
+++ 260108/사각형의 총 넓이 2/total-width-of-a-rectangle2.py
@@ -9,18 +9,16 @@ for _ in range(n):
 
 # Please write your code here.
 
-field = [[0] * 200]*200
+field = [[0] * 200 for _ in range(200)]
 ans = 0
 
-for i in range(n):
-    for a, b, c, d in zip(x1, y1, x2, y2):
-        for x in range(a+100, c+100):
-            for y in range(b+100, d+100):
-                cur_point = field[x][y]
-                if cur_point == 0:
-                    cur_point = 1
-                    ans += 1
-                else:
-                    continue
+for a, b, c, d in zip(x1, y1, x2, y2):
+    for x in range(a+100, c+100):
+        for y in range(b+100, d+100):
+            if field[x][y] == 0:
+                field[x][y] = 1
+                ans += 1
+            else:
+                continue
 
 print(ans)
