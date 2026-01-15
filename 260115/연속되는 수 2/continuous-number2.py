c4cf17ba8c09a0039cf265408c07735d679df0a0
Taekyung Ahn <46814961+gaetokk@users.noreply.github.com>
2026-01-15 21:47:09 +0900

    Title: 연속되는 수 2, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 260115/연속되는 수 2/continuous-number2.py
+++ 260115/연속되는 수 2/continuous-number2.py
@@ -2,16 +2,21 @@ n = int(input())
 arr = [int(input()) for _ in range(n)]
 
 # Please write your code here.
-cnt = 1
+cnt_max = 0
+cur_max = 0
 
 for i in range(n):
     if i == 0:
-        continue
+        cur_max = 1
     else:
-        if arr[i] != arr[i-1]:
-            cnt += 1
+        if arr[i] == arr[i-1]:
+            cur_max += 1
+            cnt_max = max(cnt_max, cur_max)
+        else:
+            cur_max = 1
 
 
-print(cnt)
+
+print(cnt_max)
 
         
