24e60494aaa7b175741ca0dfdb0b08f208094ec6
Taekyung Ahn <46814961+gaetokk@users.noreply.github.com>
2026-01-22 23:11:30 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 48ms, Memory: 16MB, Status: Passed - Codetree

--- 260122/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
+++ 260122/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -1,32 +1,38 @@
 N, K, P, T = map(int, input().split())
-handshakes = [tuple(map(int, input().split())) for _ in range(T)]
+handshakes = []
+for _ in range(T):
+    t, x, y = map(int, input().split())
+    handshakes.append((t, x, y))
 
-# Please write your code here.
-is_sick = [0] * (N + 1)
-is_sick_2 = [0] * (N + 1)
-is_sick_2[P] = 1
+# 시간 순으로 정렬
+handshakes.sort()
 
-hs_cnt = [0] * 1000
+# 감염 여부
+infected = [False] * (N + 1)
+infected[P] = True
 
-for i in range(T):
-    t, x, y = handshakes[i]
-    hs_cnt[t] = (x, y)
+# 감염자가 악수한 횟수 (감염자가 아니면 의미 없음)
+shake_count = [0] * (N + 1)
 
-for tm in hs_cnt:
-    if tm != 0:
-        x, y = tm
-        if is_sick_2[x] > 0 and is_sick[x] < K:
-            is_sick[x] += 1
-            is_sick[y] += 1
-            is_sick_2[y] = 1
-        elif is_sick_2[y] > 0 and is_sick[y] < K:
-            is_sick[x] += 1
-            is_sick_2[x] = 1
-            is_sick[y] += 1
+for t, x, y in handshakes:
+    # 악수 전 상태 저장 (중요!)
+    x_was_infected = infected[x]
+    y_was_infected = infected[y]
+    x_can_spread = x_was_infected and shake_count[x] < K
+    y_can_spread = y_was_infected and shake_count[y] < K
+    
+    # 전염 처리
+    if x_can_spread:
+        infected[y] = True
+    if y_can_spread:
+        infected[x] = True
+    
+    # 악수 횟수 증가 (감염자였던 경우만)
+    if x_was_infected:
+        shake_count[x] += 1
+    if y_was_infected:
+        shake_count[y] += 1
 
-
-for i in range(1, N+1):
-    if is_sick[i] > 0:
-        print(1, end='')
-    else:
-        print(0, end='')
+# 출력
+for i in range(1, N + 1):
+    print(1 if infected[i] else 0, end='')
