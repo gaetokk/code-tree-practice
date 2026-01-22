f5a01159f8df965233a8dbfdb5a8eb5abc47b327
Taekyung Ahn <46814961+gaetokk@users.noreply.github.com>
2026-01-22 22:59:40 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 79ms, Memory: 18MB, Status: Runtime Error - Codetree

---
+++ 260122/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -0,0 +1,32 @@
+N, K, P, T = map(int, input().split())
+handshakes = [tuple(map(int, input().split())) for _ in range(T)]
+
+# Please write your code here.
+is_sick = [0] * (N + 1)
+is_sick_2 = [0] * (N + 1)
+is_sick_2[P] = 1
+
+hs_cnt = [0] * 250
+
+for i in range(T):
+    t, x, y = handshakes[i]
+    hs_cnt[t] = (x, y)
+
+for tm in hs_cnt:
+    if tm != 0:
+        x, y = tm
+        if is_sick_2[x] > 0 and is_sick[x] < K:
+            is_sick[x] += 1
+            is_sick[y] += 1
+            is_sick_2[y] = 1
+        elif is_sick_2[y] > 0 and is_sick[y] < K:
+            is_sick[x] += 1
+            is_sick_2[x] = 1
+            is_sick[y] += 1
+
+
+for i in range(1, N+1):
+    if is_sick[i] > 0:
+        print(1, end='')
+    else:
+        print(0, end='')
