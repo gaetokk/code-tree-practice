81495e394118d356d2aa9ac30be8e6a53afa8c4b
Taekyung Ahn <46814961+gaetokk@users.noreply.github.com>
2026-02-06 00:27:41 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 260206/빙빙 돌며 숫자 사각형 채우기/snail-number-square.py
+++ 260206/빙빙 돌며 숫자 사각형 채우기/snail-number-square.py
@@ -3,10 +3,9 @@ arr = [[0] * m for _ in range(n)]
 
 # Please write your code here.
 answer = [
-    [0] * n
-    for _ in range(m)
+    [0] * m
+    for _ in range(n)
 ]
-
 
 def in_range(x, y):
     return 0 <= x and x < n and 0 <= y and y < m
