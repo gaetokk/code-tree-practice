6efce3cfb5c2fde360d5f9ca9442ebd8beff2b61
Taekyung Ahn <46814961+gaetokk@users.noreply.github.com>
2026-02-16 00:02:40 +0900

    Title: 모이자, Time: 52ms, Memory: 17MB, Status: Wrong Answer - Codetree

---
+++ 260216/모이자/gather.py
@@ -0,0 +1,14 @@
+n = int(input())
+A = list(map(int, input().split()))
+
+# Please write your code here.
+min_dist = 10000
+for i in range(n):
+    i_dist = 0
+    for k in range(n):
+        if k != i:
+            i_dist += abs(k - i) * A[k]
+    min_dist = min(min_dist, i_dist)
+
+
+print(min_dist)
